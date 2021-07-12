08c60359dd21671242ac823863670a4afbb872cd
Sergei1995 <mbwamwitu@mail.ru>
2021-07-12 22:40:49 +0300

    PageObject

--- Taska-1.js
+++ Taska-1.js
@@ -1,12 +1,12 @@
 const { check } = require("prettier");
-const { Builder, By, Key, until, WebElement } = require("selenium-webdriver");
-const { elementIsVisible } = require("selenium-webdriver/lib/until");
+const { Builder, By, Key, until, WebElement, elementIsVisible } = require("selenium-webdriver");
+//const { elementIsVisible } = require("selenium-webdriver/lib/until");
 
 const driver = new Builder()
     .forBrowser("firefox")
     .build();
 async function taska1() {
-    try {
+    // try {
         await driver.get("http://google.com")
         await driver.findElement(By.name("q")).sendKeys("белорусская железная дорога"
             , Key.RETURN).click;
@@ -16,9 +16,9 @@ async function taska1() {
             ("//*[@class='top-lang__item']/a[contains(text(), 'ENG')]")).click();
         await driver.findElement(By.css
                 ('[alt = "Национальный правовой портал РБ"]')).isDisplayed();
-    } catch (error) {
-        console.log(error)
-    }
+    // } catch (error) {
+    //     console.log(error)
+    // }
     driver.quit()
 }
 taska1();
