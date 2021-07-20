fa3c4ddf5369fe55fb8acfb6a10dec2b42beb804
Sergei-sych <mbwamwitu@mail.ru>
2021-07-20 13:32:43 +0300

    The last version

--- test/suit.js
+++ test/suit.js
@@ -4,40 +4,40 @@ const railway_page_search = require('../pageobject/railway_page_search');
 const railway_page_train = require('../pageobject/railway_page_train');
 describe('Suit', () => {
 
-    // it('Test-1', async function() {
-    //     this.timeout(50000);
-    //     var baseurl = 'http://google.com';
-    //     await google_page.go_to_url(baseurl);
-    //     await google_page.enter_search('белорусская железная дорога');
-    //     await google_page.go_to_railwayPage();
-    //     await railway_page_main.switchToEng();
-    //     await railway_page_main.checkElement();   
-    // });
+    it('Test-1', async function() {
+        this.timeout(50000);
+        var baseurl = 'http://google.com';
+        await google_page.go_to_url(baseurl);
+        await google_page.enter_search('белорусская железная дорога');
+        await google_page.go_to_railwayPage();
+        await railway_page_main.switchToEng();
+        await railway_page_main.checkElement();   
+    });
 
-    // it('Test-2', async function()  {
-    //     this.timeout(50000);
-    //     var baseurl = 'http://google.com';
-    //     await google_page.go_to_url(baseurl);
-    //     await google_page.enter_search('белорусская железная дорога');
-    //     await google_page.go_to_railwayPage();
-    //     await railway_page_main.switchToEng();
-    //     await railway_page_main.checkNews();
-    //     await railway_page_main.checkElement_1();
-    //     await railway_page_main.checkButton();
-    // });
+    it('Test-2', async function()  {
+        this.timeout(50000);
+        var baseurl = 'http://google.com';
+        await google_page.go_to_url(baseurl);
+        await google_page.enter_search('белорусская железная дорога');
+        await google_page.go_to_railwayPage();
+        await railway_page_main.switchToEng();
+        await railway_page_main.checkNews();
+        await railway_page_main.checkElement_1();
+        await railway_page_main.checkButton();
+    });
 
-    // it('Test-3', async function()  {
-    //     this.timeout(50000);
-    //     var baseurl = "https://www.rw.by/";
-    //     await google_page.go_to_url(baseurl);
-    //     await railway_page_main.passtheLink();
-    //     await railway_page_main.firstLink();
-    //     await railway_page_main.checkTheSame();
-    //     await railway_page_search.youSeeTheText('К сожалению, на ваш поисковый запрос ничего не найдено.');
-    //     await railway_page_search.clearText();
-    //     await railway_page_search.newText("Санкт-Петербург");
-    //     await railway_page_search.checkTheLink();
-    // });
+    it('Test-3', async function()  {
+        this.timeout(50000);
+        var baseurl = "https://www.rw.by/";
+        await google_page.go_to_url(baseurl);
+        await railway_page_main.passtheLink();
+        await railway_page_main.firstLink();
+        await railway_page_main.checkTheSame();
+        await railway_page_search.youSeeTheText('К сожалению, на ваш поисковый запрос ничего не найдено.');
+        await railway_page_search.clearText();
+        await railway_page_search.newText("Санкт-Петербург");
+        await railway_page_search.checkTheLink();
+    });
 
     it('Test-4', async function() {
         this.timeout(50000);
